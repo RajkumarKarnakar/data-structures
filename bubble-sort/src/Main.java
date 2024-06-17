0d786a3df43cfa56c3eba0dc0d8308924760e3cc
planetofthecode <rajkumar.karnakar@outlook.com>
2024-06-17 16:54:37 +0530

    bubble sort algorithm

--- bubble-sort/src/Main.java
+++ bubble-sort/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr;
+        arr = new int[]{10, 20, 15, 35, 5, 25, 45, 0, 40, -5};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] a) {
+        int temp = 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                temp = a[j];
+                if (temp > a[j + 1]) {
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+
+            }
+        }
     }
 }
--- greatest-common-divisor/src/Main.java
+++ greatest-common-divisor/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Greatest common divisor of 60 & 20 is: " + greatestCommonDivisor(375, 30));
+        System.out.println("Greatest common divisor: " + greatestCommonDivisor(1080, 308));
     }
 
     public static int greatestCommonDivisor(int m, int n) {
