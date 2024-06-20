66c07c4c179725c3bb6bdea7fd6baa3265f95754
planetofthecode <rajkumar.karnakar@outlook.com>
2024-06-20 23:19:33 +0530

    min method in recursion

--- recursion/src/Main.java
+++ recursion/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {10, 15, 78,-1000, 45, 9, 5, 88, 85,125, 22,200, 56, 99, 0};
-        System.out.println(maxOfArray(arr, arr.length));
+        int[] arr = {10, 15, 78, -1000, 45, 9, 5, 88, 85, 125, 22, 200, 56, 99, 0};
+        System.out.println("max is: " + maxOfArray(arr, arr.length));
+        System.out.println("Min is: " + minOfArray(arr, arr.length));
     }
 
     static int maxOfArray(int[] a, int n) {
         if (n == 1) return a[0];
         return Math.max(a[n - 1], maxOfArray(a, n - 1));
+    }
+
+    static int minOfArray(int[] a, int n) {
+        if (n == 1) return a[0];
+        return Math.min(a[n - 1], minOfArray(a, n - 1));
     }
 }
